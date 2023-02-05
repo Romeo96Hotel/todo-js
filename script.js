2dcce9f82375c6d20e4e8621909e0c9fa0c1eb31
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-02-05 02:27:21 -0300

    Updated button name

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const getText = function () {
-    const textBox = document.querySelector("input").value;
-    return textBox;
-  };
-  
-  const appendText = function (text) {
-    const todoList = document.querySelector("#ul-container");
-    todoList.appendChild(text);
-  };
-  
-  const click = document
-    .querySelector("#text-button")
-    .addEventListener("click", () => {
-      console.log(getText.textBox);
-    });
-  
+  const textBox = document.querySelector("input").value;
+  return textBox;
+};
+
+const appendText = function (text) {
+  const todoList = document.querySelector("#ul-container");
+  todoList.appendChild(text);
+};
+
+const buttonClick = document
+  .querySelector("#text-button")
+  .addEventListener("click", () => {
+    console.log(getText.textBox);
+  });
