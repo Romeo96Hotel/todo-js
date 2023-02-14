9d7a22ac84132f44e58373463cb335eae4c675ff
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-02-14 11:34:38 -0300

    Starting over with MVC approach

--- script.js
+++ script.js
@@ -3,42 +3,9 @@ User types in text to append
 Presses button
 returns div with text + remove button
  */
-const textField = document.querySelector("#input-text-field");
-const container = document.getElementById("ul-container");
-const appendButton = document.getElementById("input-button");
 
-const todo = (function (text, button) {
-  /* function initilization */
-  function init() {
-    button.addEventListener("click", () => {
-      getText;
-    });
-  }
+class Model {}
 
-  /* helper functions */
+class View {}
 
-  const itemContainer = () => {
-    const newDiv = document.createElement("div");
-    return newDiv.style.
-  };
-  const textDiv = () => document.createElement("div");
-  const buttonDiv = () => document.createElement("div");
-
-  function addListElement() {
-    const textNode = document.createElement("h3");
-
-    textNode.textContent = text.value;
-    container.appendChild(textNode);
-  }
-
-  function addDelButton() {
-    const delButton = document.createElement("button");
-    container.append(delButton);
-  }
-
-  /* Returns automatically */
-  return {
-    name: "todo",
-    init: init(),
-  };
-})(textField, appendButton);
+class Controller {}
