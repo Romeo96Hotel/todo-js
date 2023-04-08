8ecf734912cd4d5d46d782461e8a715efa3c9d51
alexyspol <alexsandroyspol@gmail.com>
2023-04-08 01:31:27 -0300

    Add basic Todo List functionality
    This commit adds basic functionality to a Todo List application. The code initializes the todoList array, creates a controller object with methods for initializing the view, adding items to the list, toggling items as completed, and deleting items from the list. The view object has methods for initializing the DOM elements, updating the view, and getting the input text from the user.
    
    The code also creates a Todo object that returns the list of items and initializes the controller object. When the user interacts with the DOM elements, the controller object calls the appropriate method and updates the view.

--- script.js
+++ script.js
@@ -1,44 +1,100 @@
-/* 
-User types in text to append
-Presses button
-returns div with text + remove button
- */
-const textField = document.querySelector("#input-text-field");
-const container = document.getElementById("ul-container");
-const appendButton = document.getElementById("input-button");
+'use strict';
 
-const todo = (function (text, button) {
-  /* function initilization */
-  function init() {
-    button.addEventListener("click", () => {
-      getText;
-    });
-  }
+let todo = (() => {
 
-  /* helper functions */
+  let todoList = [];
 
-  const itemContainer = () => {
-    const newDiv = document.createElement("div");
-    return newDiv.style.
+  const controller = {
+    init() {
+      view.init();
+    },
+
+    onAppendButtonClicked() {
+      const text = view.getText();
+
+      // If the text is empty, don't add it to the list of todo's and exit.
+      if(text.length === 0) return;
+
+      let todoItem = {
+        text: text,
+        completed: false
+      };
+
+      todoList.push(todoItem);
+
+      view.update();
+    },
+
+    getItems() {
+      return todoList;
+    },
+
+    onCompleteButtonClicked() {
+      const i = parseInt(this.dataset.index);
+
+      // Toggle the 'completed' property of the corresponding todo item
+      todoList[i].completed = !todoList[i].completed;
+
+      view.update();
+    },
+
+    onDeleteButtonClicked() {
+      // Filter out the todo item with the corresponding index
+      todoList = todoList.filter((val, i) => {
+        if(i !== parseInt(this.dataset.index)) return val
+      });
+      
+      view.update();
+    }
   };
-  const textDiv = () => document.createElement("div");
-  const buttonDiv = () => document.createElement("div");
 
-  function addListElement() {
-    const textNode = document.createElement("h3");
+  const view = {
+    // This method initializes the view by getting references to the text field, append button, and todo list
+    init() {
+      view.$textField = document.querySelector('#input-text-field');
+      view.$appendButton = document.querySelector('#append-button');
+      view.$appendButton.addEventListener('click', controller.onAppendButtonClicked);
+      view.$todoList = document.querySelector('#todo-list');
+    },
 
-    textNode.textContent = text.value;
-    container.appendChild(textNode);
-  }
+    getText() {
+      return view.$textField.value;
+    },
 
-  function addDelButton() {
-    const delButton = document.createElement("button");
-    container.append(delButton);
-  }
+    // This method updates the view by clearing the text field and todo list, and then adding each todo item to the list
+    update() {
+      view.$textField.value = '';
+      view.$todoList.innerHTML = '';
 
-  /* Returns automatically */
+      // Loop through each todo item and create a list item with complete and delete buttons
+      controller.getItems().forEach((todoItem, i) => {
+        
+        let $completeButton = document.createElement('button');
+        $completeButton.innerText = 'completed';
+        $completeButton.dataset.index = i;
+        $completeButton.addEventListener('click', controller.onCompleteButtonClicked);
+        
+        let $deleteButton = document.createElement('button');
+        $deleteButton.innerText = 'delete';
+        $deleteButton.dataset.index = i;
+        $deleteButton.addEventListener('click', controller.onDeleteButtonClicked);
+        
+        let $listItem = document.createElement('li');
+        $listItem.innerText = todoItem.text;
+        $listItem.dataset.completed = todoItem.completed;
+
+        $listItem.appendChild($completeButton);
+        $listItem.appendChild($deleteButton);
+        view.$todoList.appendChild($listItem);
+      });
+    }
+  };
+
   return {
-    name: "todo",
-    init: init(),
-  };
-})(textField, appendButton);
+    items: controller.getItems,
+    init: controller.init
+  }
+
+})()
+
+todo.init();
